91d653ab27db770bc6c938ff64c4a33d710584d7
Alex Noll <nollalexander@yahoo.com>
2019-11-22 21:15:51 -0500

    Add files via upload

--- FinalProjectAdventureGame.py
+++ FinalProjectAdventureGame.py
@@ -59,13 +59,46 @@ elif launch_option == "2":
         cell_voice_option = input('>')
         if cell_voice_option == "1":
             print("You make up a complex story of grandeur about saving a top politician from an assassination attempt and then being framed for the assassination attempt yourself")
+            print('The inmate manages to suspend their disbelief, calling you a "fucking liar" and threatens to bash your skull in for lying to them\n\nDo you:\n\n#1-Try to explain yourself,\n\n#2-Challenge them to a fight,\n\n or #3-Just leave it be?' )
+            lie_inmate_option = input('>')
+            if lie_inmate_option == "1":
+                print("You explain yourself to the inmate and pacify the situation, now trusting you the inmate tells you why they're in the can too: \n\nA repeat drug smuggler across the border, caught on a cocaine run this time, they proclaim, my name's Wyatt, holler through if you need anything\n\nFeeling like you've made a prison buddy, you both decide to go to sleep for the night.")
+                print('You wake up to the sound of gunshots, BANG, BANG, BANG, followed quickly by short, blood-curdling screams, quickly extinguished by more gunshots.\n\n\n\nFrightened, you try to hide yourself in your cell.\n\nLooking across, you finally see Wyatt, standing directly in front of the cell door.\n\nNoticing you staring at him, he looks over as you mouth for him to hide, moments later, three armed mercenaries come over to his door and shoot the lock, freeing him.\n\nIt was all part of his plan, Wyatt knew he would not be in for long')
+            elif lie_inmate_option == "2":
+                print("You decide that you're not gonna put up with their shit, you call them on their claim and say you'll kill them before they even get the chance, you two argue through the night until you eventually pass out at the window.")
+            elif lie_inmate_option == "3":
+                print("You decide the best action is inaction, this further infuriates the inmate, they begin throwing sand through the window along with repeated screams of profanity, causing a guard to come investigate")
         elif cell_voice_option == "2":
             print("You tell them that you quite simply just don't know, you woke up in this prison cell and that's all that you know")
+            print('Seemingly unsatisfied with your response, the inmate responds with: \n\n"REALLY, how do you wind up here and not know, what are you, STUPID?"\n\nEventually the fellow inmate comes to realize that you might not want to talk or genuinely do not remember, either way they leave you alone and you drift off to sleep.')
         elif cell_voice_option == "3":
-            print("You tell them you robbed bank, you go into immense detail with how you did it, exactly what happened, and how you were caught.")
+            print("You tell them you robbed a bank, you go into immense detail with how you did it, exactly what happened, and how you were caught.")
+            print("The inmate, seemingly immersed in your story begins to ask you question after question, fascinated with all of the ins and outs of your story and how you wound up in prison with them.\n\nThings seem to be going well and the inmate tells you how they got in: Smuggling cocaine across the border\n\nFeeling like you've made a prison pal, you both decide to go to sleep")
         elif cell_voice_option == "4":
             print("You tell them that you murdered someone and were caught doing it.")
+            print("Immediately the inmate wants to know all of the details, who, how, and why? \n\nDo you tell them -#1 \n\nor \n\n#2-Say that you'd rather not get into it?")
+            murder_inmate_choice = input('>')
+            if murder_inmate_choice == "1":
+                print("You decide to tell them the full story: You were riding your horse through Redmonton on your way to running an errand when a local drunk came wandering out of the salty spitoon looking for a fight.\n\nDespite the fact that you're just riding your horse through town, they draw their revolver and aim it sloppily at your horse.\n\nWithout hesitation, you pull your piece and shoot them straight through the eye.\n\nBecause of the commotion townspeople run over and see the dead drunkard on the saloon steps.\n\nImmediately you're placed under arrest and thrown in jail, and here you are.")
+                print('The inmate responds with: "Wow, that is some fucking bullshit, sorry; I would do the exact same though, I have in fact"\n\nYou talk more together and they tell you what they are in for: Smuggling cocaine across the border\n\nFeeling like you have made a prison buddy, you both decide to go to sleep.')
+            elif murder_inmate_choice == "2":
+                print("Deciding that you don't want to get into it, you leave the inmate guessing, annoyed but somewhat satisfied, they leave you alone for the night, you walk over to your bedroll and go to sleep.")
         elif cell_voice_option == "5":
             print("You tell them that you did things that can't be spoken of without serious repercussion, the things you did were horrible, unspeakable.")
+            print('You can hear the sudden increased breathing rate of your fellow inmate as they mutter "What the hell is wrong with you?", your inmate is already obviously spooked, do you \n\n#1-Try to frighten them? \n\n Or \n\n#2-Calm them down?')
+            spooked_inmate = input('>')
+            if spooked_inmate == "1":
+                print('You continue to frighten the inmate until they scream "Stay the fuck away from me you freak, you hear me!"\n\nSatisfied with yourself for the night, you go to sleep comfortably in your bedroll')
+            elif spooked_inmate == "2":
+                print("You decide you've had enough fun terrorizing your fellow inmate and strive to calm them down.\n\nYou explain yourself and they immediately seem calmer, you two share a conversation where they tell you how they got in: Smuggling cocaine across the border\n\nFeeling like you've made a prison pal, you both decide to go to sleep for the night.")
         elif cell_voice_option == "6":
             print("Words are hard, despite your best efforts all you can manage is a shrill whimper.")
+            print("The inmate laughs at you non-stop, you walk over to your bedroll and cry yourself to sleep")
+    elif prison_option == "2":
+        print('The fellow inmate is angered by your lack of your response and starts screaming\N\N"THE SILENT TYPE HUH, I AM GONNA KILL THE LIFE OUT OF YOU, I WONDER IF YOULL BE SILENT THEN"')
+        print("The inmate begins throwing shaking the bars to the window between your cells, suddenly you realize they're powerless and they can't do anything to you, presenting you with two options\n\nDo you:\n\n#1-Go over to the window and punch their hands?\n\nOr\n\n#2-Sit back in your cell and go to sleep?")
+        silence_option_inmate = input('>')
+        if silence_option_inmate == "1":
+            print('You decide to go over to the window bars and smash the inmates fingers against them, they let out a single shrill scream and call you a "fucking asshole" before retreating across their room\n\nIn a much higher pitched voice than last used, they say "Im gonna get you for this, just wait"\n\nUnimpressed, you decide to walk back to your bedroll and go to sleep for the night.')
+        elif silence_option_inmate == "2":
+            print('You decide to ignore them and just go to sleep in your bedroll. Upset by your lack of attention, the inmate begins scheming.')
